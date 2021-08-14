61f5362ac257f1a9da2361cc9baef889d39c547c
Masahiro Nagata <opampg@gmail.com>
2021-08-14 21:08:31 +0900

    MainWindowのサイズを大きめの仮サイズに設定

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,12 @@
 #include <libguile.h>
 #include "MainWindow.hpp"
 
-// static SCM my_hostname(){
-//     return scm_from_locale_string ("My-host-name-dummy");
-// }
-
-// static void* imain(void *data){
-//   scm_c_define_gsubr("my-hostname", 0, 0, 0, (scm_t_subr)my_hostname);
-//   SCM st2 = scm_from_locale_string("(/ 1 0)");
-//   scm_eval_string(st2);
-//   SCM st = scm_from_locale_string("hello world\n");
-//   SCM out = scm_current_output_port();
-//   scm_display(st, out);
-//   return NULL;
-// }
-
-
 int main(int argc, char** argv) {
-  // scm_with_guile(imain, 0);
-  // return 0;
   QApplication app(argc, argv);
   app.setApplicationName("GuiREPL");
 
   MainWindow* window = new MainWindow();
+  window->resize(900,500); // 仮のサイズ
   window->show();
 
   return app.exec();
